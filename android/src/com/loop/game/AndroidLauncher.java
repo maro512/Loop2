981ed36a90c275015f8417ce1812897df8889cd7
Piotr <piotr.pikul@student.uj.edu.pl>
2017-05-21 12:27:46 +0200

    Do testu SSL na Androidzie

--- android/src/com/loop/game/AndroidLauncher.java
+++ android/src/com/loop/game/AndroidLauncher.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.loop.game.LoopGame;
+import com.loop.game.net.ClientTest;
+import com.loop.game.net.LoopServer;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -12,5 +14,15 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new LoopGame(), config);
+		try
+		{
+			ClientTest.run(
+                    LoopServer.getSSLContext(getResources().openRawResource(
+                            getResources().getIdentifier("trusta","raw", getPackageName())),new char[]{'l', 'o', 'o', 'p', '2', '0', '1', '7'})
+                    ,new String[0]);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
